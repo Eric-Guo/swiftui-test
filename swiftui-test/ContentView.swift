5acf7ae907f17ac17249350849fdf6f69747cd10
Eric-Guo <eric.guocz@gmail.com>
2020-09-25 09:39:50 +0800

    Initial Commit

---
+++ swiftui-test/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  swiftui-test
+//
+//  Created by 过纯中 on 2020/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
